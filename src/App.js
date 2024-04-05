335c2d39dd3fe38c7f8ffacff9b916f2832b9011
Krizz1103 <132948667+Krizz1103@users.noreply.github.com>
2024-04-05 21:32:53 +0100

    Updated Github address

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ function App() {
       <Search />
       <footer>
         This project was coded by Kristin Kessler and is{" "}
-        <a href="https://github.com/Krizz1103/react-week-4-homework">
+        <a
+          href="https://github.com/Krizz1103/react-week-4-homework-final"
+          target="_blank"
+          rel="noreferrer"
+        >
           {" "}
           open-source code
         </a>{" "}
-        on Github and hosted on <a href="">Netlify</a>
+        on Github and hosted on Netlify
       </footer>
     </div>
   );
